64380cb2e2991741e28fa5413e11aaf578a501b5
sophal111323 <phaltp1@gmail.com>
2025-06-29 21:14:12 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function goTo(platform) {
+    const links = {
+        facebook: "https://www.facebook.com/pha.mazer.2025",
+        tiktok: "https://www.tiktok.com/@an1thephal",
+        youtube: "https://youtube.com/@itzthephal?si=egjjjbhLdHITLF3k"
+        telegram: "https://t.me/Thephal"
+    };
+    window.open(links[platform], "_blank");
+}
